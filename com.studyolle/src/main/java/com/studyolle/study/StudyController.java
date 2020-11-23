34cc520a489ad5e778d22a5b8ffc4688cb1ee69a
user-PC\user <ko>
2020-11-23 21:25:33 +0900

    스터디 설정 - 배너

--- com.studyolle/src/main/java/com/studyolle/study/StudyController.java
+++ com.studyolle/src/main/java/com/studyolle/study/StudyController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.net.URLEncoder;
@@ -66,6 +67,27 @@ public class StudyController {
         model.addAttribute(account);
         model.addAttribute(study);
         return "study/members";
+    }
+
+    @GetMapping("/banner")
+    public String studyImageForm(@CurrentAccount Account account, @PathVariable String path, Model model) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        model.addAttribute(account);
+        model.addAttribute(study);
+        return "study/settings/banner";
+    }
+
+    @PostMapping("/banner")
+    public String studyImageSubmit(@CurrentAccount Account account, @PathVariable String path,
+                                   String image, RedirectAttributes attributes) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        studyService.updateStudyImage(study, path);
+        attributes.addFlashAttribute("message", "스터디 이미지 수정 완료");
+        return "redirect:/study/" + getPath(path) + "/settings/banner";
+    }
+
+    private String getPath(String path) {
+        return URLEncoder.encode(path, StandardCharsets.UTF_8);
     }
 
 }
